ba919ba8f7bb781769dc46239ee5a2c8a3945a0d
Mark <meichangliang@outlook.com>
2019-05-14 19:43:45 +0800

    ~

--- src/config/main.tsx
+++ src/config/main.tsx
@@ -3,16 +3,20 @@
  * @Description: none
  * @Author: Mark
  * @Date: 2019-05-14 16:35:05
- * @LastEditTime: 2019-05-14 19:37:29
+ * @LastEditTime: 2019-05-14 19:43:24
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import { printVersion } from './baseUrl';
 import 'react-app-polyfill/ie9';
 import 'normalize.css';
-import App from '@/App';
+import '@/assets/style/reset.less';
+import BaseRouter from './BaseRouter';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+printVersion();
+
+ReactDOM.render( <BaseRouter />, document.getElementById( 'root' ) );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
