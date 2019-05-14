4a9668ee60af1efae95a490e9503916634956aca
Mark <meichangliang@outlook.com>
2019-05-14 17:53:20 +0800

    去除css\~

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,15 @@
  * @Description: none
  * @Author: Mark
  * @Date: 2019-05-14 16:35:05
- * @LastEditTime: 2019-05-14 17:33:07
+ * @LastEditTime: 2019-05-14 17:53:12
  */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
--- src/config/main.tsx
+++ src/config/main.tsx
@@ -3,12 +3,12 @@
  * @Description: none
  * @Author: Mark
  * @Date: 2019-05-14 16:35:05
- * @LastEditTime: 2019-05-14 17:52:02
+ * @LastEditTime: 2019-05-14 17:51:59
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from '../App';
+import * as serviceWorker from '../serviceWorker';
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 
