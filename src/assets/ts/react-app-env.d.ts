003457bc816eab47b5e71211fdc75a8062763cf5
Mark <meichangliang@outlook.com>
2019-05-14 19:50:22 +0800

    添加less 支持~

--- src/assets/ts/react-app-env.d.ts
+++ src/assets/ts/react-app-env.d.ts
@@ -43,7 +43,7 @@ declare module '*.svg' {
   import * as React from 'react';
 
   export const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement>
+  React.SVGProps<SVGSVGElement>
   >;
 
   const src: string;
@@ -61,6 +61,11 @@ declare module '*.module.scss' {
 }
 
 declare module '*.module.sass' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.less' {
   const classes: { [key: string]: string };
   export default classes;
 }
--- src/config/BaseRouter.tsx
+++ src/config/BaseRouter.tsx
@@ -3,25 +3,18 @@
  * @Description: none
  * @Author: Mark
  * @Date: 2019-05-14 16:35:05
- * @LastEditTime: 2019-05-14 17:53:12
+ * @LastEditTime: 2019-05-14 19:49:38
  */
 import React from 'react';
+import styles from '../test.module.less';
+import '../test.less';
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.wrapper}>
+      <div className="wrapper">我们都是一家人</div>
+      <div>我们都是一家人</div>
+      <div>我们都是一家人</div>
+      <div>我们都是一家人</div>
     </div>
   );
 };
