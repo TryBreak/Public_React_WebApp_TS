80d46d7253e7d42adf5d64a18b5878d2a598faa9
Mark <meichangliang@outlook.com>
2019-05-15 14:40:13 +0800

    ~

--- src/pages/Demo/index.tsx
+++ src/pages/Demo/index.tsx
@@ -1,12 +1,40 @@
+/*
+ * @LastEditors: Mark
+ * @Description: none
+ * @Author: Mark
+ * @Date: 2019-05-05 11:53:31
+ * @LastEditTime: 2019-05-15 14:39:07
+ */
 import React, { Component } from 'react';
-class Home extends Component {
+import { Link /* ,Route */ } from 'react-router-dom';
+import RouteView from '@/config/RouteView';
+import styles from './index.module.less';
+
+class Demo extends Component {
   render () {
+    const { match }: any = this.props;
     return (
-      <div>
-        <h1>这里是Demo </h1>
+      <div className="Demo">
+        <h2 className={styles.title}>Demo</h2>
+        <ul>
+          <li>
+            <Link to="/demo/style_demo">style_demo</Link>
+          </li>
+          <li>
+            <Link to="/demo/request_demo">request_demo</Link>
+          </li>
+          <li>
+            <Link to="/demo/mobox_demo">mobox_demo</Link>
+          </li>
+        </ul>
+        <RouteView match={match} />
+        {/* <Route
+          exact
+          path={match.path}
+          render={() => <h3>这里是demo首页哦</h3>}
+        /> */}
       </div>
     );
   }
 }
-
-export default Home;
+export default Demo;
