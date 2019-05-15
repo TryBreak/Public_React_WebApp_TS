dab1f237bb18074eb6ca6509248f750b5d23c10c
Mark <meichangliang@outlook.com>
2019-05-15 14:28:13 +0800

    update~

--- src/config/BaseRouter.tsx
+++ src/config/BaseRouter.tsx
@@ -3,7 +3,7 @@
  * @Description: none
  * @Author: Mark
  * @Date: 2019-05-14 16:35:05
- * @LastEditTime: 2019-05-15 13:45:49
+ * @LastEditTime: 2019-05-15 14:28:04
  */
 import React, { Component } from 'react';
 
@@ -16,8 +16,39 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Inbox from './Inbox'; //页面集合全路由展示
 import RouteView from './RouteView'; //路由渲染文件
+import { project_detail } from '@/config/constants';
+import { fondRoute, splitPath /* ,storagePath */ } from '@/utils/inspectRouter';
 
 class App extends Component {
+  // constructor ( props: any ) {
+  //   super( props );
+  //   $axios_set_default();
+  // }
+  componentDidMount () {
+    this.watchRouter();
+  }
+  componentWillUpdate () {
+    this.watchRouter();
+  }
+  titleAnd404 = ( pathname: any ) => {
+    // storagePath( ( this.props as any ).history, pathname );
+
+    const nowRouter = fondRoute( pathname );
+    const nowPath = splitPath( pathname );
+    if ( nowRouter ) {
+    } else if ( nowPath[0] !== '/inbox' ) {
+      ( this.props as any ).history.replace( '/404' );
+    }
+    const title = nowRouter && nowRouter.title;
+    if ( title ) {
+      window.document.title = title || project_detail.name;
+    }
+  };
+  //全局的路由变化监听
+  watchRouter = () => {
+    const { pathname } = ( this.props as any ).history.location;
+    this.titleAnd404( pathname );
+  };
   render () {
     return (
       <Switch>
---
+++ src/config/constants.ts
@@ -0,0 +1,8 @@
+/*
+ * @LastEditors: Mark
+ * @Description: 全局的参数
+ * @Author: Mark
+ * @Date: 2019-05-05 11:53:31
+ * @LastEditTime: 2019-05-15 14:26:27
+ */
+export const project_detail = { name: process.env.REACT_APP_PROJECT_NAME };
