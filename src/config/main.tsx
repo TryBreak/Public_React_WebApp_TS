f9ba1d8ba36ad0586c199b2c7226c80d8e396331
Mark <meichangliang@outlook.com>
2019-05-25 15:18:50 +0800

    路由变化~

--- src/config/main.tsx
+++ src/config/main.tsx
@@ -3,7 +3,7 @@
  * @Description: none
  * @Author: Mark
  * @Date: 2019-05-14 16:35:05
- * @LastEditTime: 2019-05-25 15:07:16
+ * @LastEditTime: 2019-05-25 15:17:49
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -11,7 +11,7 @@ import * as serviceWorker from './serviceWorker';
 import { printVersion } from './baseUrl';
 import 'react-app-polyfill/ie9';
 import 'normalize.css';
-import '@/assets/style/global.scss';
+import '@/assets/style/reset.scss';
 import BaseRouter from './BaseRouter';
 
 printVersion();
